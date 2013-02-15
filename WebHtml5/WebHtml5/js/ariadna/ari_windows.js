532e6962057ec0fd9ce50f982a69b510bed06aa3
Rafael Garcia <ragare@gmail.com>
2013-02-15 04:45:13 +0100

    Fase 1:
    Páginas informativas y de navegación

---
+++ WebHtml5/WebHtml5/js/ariadna/ari_windows.js
@@ -0,0 +1,15 @@
+﻿function ari_openDesktop() {
+    //alert("ari_openDesktop");
+    window.open("WhatDoIDoPage.html", "WHATDOIDO", "width=1024, height=690,resizable=1");
+}
+function ari_openTablet() {
+    //alert("ari_openTablet");
+    window.open("WhatDoIDoPage.html", "WHATDOIDO", "width=946, height=768,resizable=1");
+}
+function ari_openSmartphone() {
+    //alert("ari_openSmartphone");
+    window.open("WhatDoIDoPage.html", "WHATDOIDO", "width=320, height=480,resizable=1");
+}
+function ari_openSelf(url) {
+    alert("openSelf() url=" + url);
+}
