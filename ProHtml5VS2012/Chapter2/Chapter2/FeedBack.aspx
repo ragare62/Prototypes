c0e8f84714f5136e6464de7358141aef82c2eb2d
Rafael Garcia <rafa@myariadna.com>
2013-03-04 19:08:29 +0100

    Some test

---
+++ ProHtml5VS2012/Chapter2/Chapter2/FeedBack.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FeedBack.aspx.cs" Inherits="Chapter2.FeedBack" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title></title>
+    </head>
+    <body>
+        <form id="form1" runat="server">
+            <div>
+                <fieldset>
+                    <legend>Feedback Form</legend>
+                    <ol>
+                        <li>
+                            <asp:Label id="lblURL" runat="server" AssociatedControlID="URL">Default home page</asp:Label>
+                            <asp:TextBox runat="server" ID="URL" TextMode="Url"></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblOptions" runat="server">Default browser</asp:Label>
+                            <select name="Options" id="Options">
+                                <option value="1" > Internet Explorer</option>
+                                <option selected="selected" value="2" > Google Chrome</option>
+                                <option value="3" > Firefox</option>
+                                <option value="4" > Opera</option>
+                            </select>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblBirthday" runat="server" AssociatedControlID="Birthday">Birthday</asp:Label>
+                            <asp:TextBox ID="Birthday" runat="server" TextMode="Date"></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblMonth" runat="server" AssociatedControlID="Month">Favorite Month</asp:Label>
+                            <asp:TextBox runat="server" ID="Month" TextMode="Month"></asp:TextBox>
+
+                        </li>
+                        <li>
+                            <asp:Label ID="lblWeek" runat="server" AssociatedControlID="Week">Busiest Week</asp:Label>
+                            <asp:TextBox runat="server" ID="Week" TextMode="Week"></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblStart" runat="server" AssociatedControlID="DateTime">Start Date/Time</asp:Label>
+                            <asp:TextBox runat="server" ID="DateTime" TextMode="DateTime"></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblTime" runat="server" AssociatedControlID="Time">Current Time</asp:Label>
+                            <asp:TextBox runat="server" ID="Time" TextMode="Time" ></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblPhone" runat="server" AssociatedControlID="Phone">Phone</asp:Label>
+                            <asp:TextBox runat="server" ID="Phone" TextMode="Phone"></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblRange" runat="server" AssociatedControlID="Range">Overall satisfaction</asp:Label>
+                            <asp:TextBox runat="server" ID="Range" TextMode="Range" Width="200" Height="30"></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblColor" runat="server" AssociatedControlID="Color">Preferred color</asp:Label>
+                            <asp:TextBox runat="server" ID="Color" TextMode="Color"></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblScore" runat="server" AssociatedControlID="Score">Overall Rating</asp:Label>
+                            <asp:TextBox ID="Score" runat="server" TextMode="Number" MaxLength="1"></asp:TextBox>
+                        </li>
+                        <li>
+                            <asp:Label ID="lblComments" runat="server" AssociatedControlID="Multi">Comments</asp:Label>
+                            <asp:TextBox runat="server" ID="Multi" TextMode="Multiline" Rows="5" Columns="30"></asp:TextBox>
+                        </li>
+                    </ol>
+                    <asp:Button ID="Submit" runat="server" CommandName="Submit" Text="Submit" />
+                </fieldset>
+            </div>
+        </form>
+    </body>
+</html>
---
+++ ProHtml5VS2012/Chapter2/Chapter2/FeedBack.aspx.designer.cs
@@ -0,0 +1,240 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Chapter2 {
+    
+    
+    public partial class FeedBack {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control lblURL.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblURL;
+        
+        /// <summary>
+        /// Control URL.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox URL;
+        
+        /// <summary>
+        /// Control lblOptions.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblOptions;
+        
+        /// <summary>
+        /// Control lblBirthday.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBirthday;
+        
+        /// <summary>
+        /// Control Birthday.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Birthday;
+        
+        /// <summary>
+        /// Control lblMonth.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMonth;
+        
+        /// <summary>
+        /// Control Month.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Month;
+        
+        /// <summary>
+        /// Control lblWeek.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblWeek;
+        
+        /// <summary>
+        /// Control Week.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Week;
+        
+        /// <summary>
+        /// Control lblStart.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStart;
+        
+        /// <summary>
+        /// Control DateTime.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox DateTime;
+        
+        /// <summary>
+        /// Control lblTime.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTime;
+        
+        /// <summary>
+        /// Control Time.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Time;
+        
+        /// <summary>
+        /// Control lblPhone.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPhone;
+        
+        /// <summary>
+        /// Control Phone.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Phone;
+        
+        /// <summary>
+        /// Control lblRange.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblRange;
+        
+        /// <summary>
+        /// Control Range.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Range;
+        
+        /// <summary>
+        /// Control lblColor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblColor;
+        
+        /// <summary>
+        /// Control Color.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Color;
+        
+        /// <summary>
+        /// Control lblScore.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblScore;
+        
+        /// <summary>
+        /// Control Score.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Score;
+        
+        /// <summary>
+        /// Control lblComments.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblComments;
+        
+        /// <summary>
+        /// Control Multi.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Multi;
+        
+        /// <summary>
+        /// Control Submit.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Submit;
+    }
+}
