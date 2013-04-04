a1fe8663ecc5de7134c44995ab367e68dfabbbd2
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2013-04-04 17:54:51 +0200

    New ticket generator

---
+++ TicketGenerator/TicketGenerator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p = "";
+            while (p != "S")
+            {
+                Console.WriteLine("Call for a key-->");
+                string ky = GenerateKey();
+                Console.WriteLine(ky);
+                p = Console.ReadLine();
+            }
+        }
+        public static string GenerateKey()
+        {
+            return GetPassword();
+        }
+        private static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+        /// <summary>
+        /// Generates a random string with the given length
+        /// </summary>
+        /// <param name="size">Size of the string</param>
+        /// <param name="lowerCase">If true, generate lowercase string</param>
+        /// <returns>Random string</returns>
+        private static string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+        public static string GetPassword()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(RandomString(4, false));
+            builder.Append(RandomNumber(1000, 9999));
+            builder.Append(RandomString(2, false));
+            return builder.ToString();
+        }
+    }
+}
